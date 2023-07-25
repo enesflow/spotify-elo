80db28d4700a1023cc6e0441aea8ccd4bed35bfa
enesflow <enes@kavcakar.com.tr>
2023-07-25 11:43:41 +0300

    awd

--- src/entry.fastify.tsx
+++ src/entry.fastify.tsx
@@ -41,7 +41,7 @@ const start = async () => {
 
 	// Start the fastify server
 	// setting the "host"
-	await fastify.listen({ port: PORT, host: "vazgec.kavcakar.tech" });
+	await fastify.listen({ port: PORT, host: "0.0.0.0" });
 };
 
 start();
